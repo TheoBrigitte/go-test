e977e662e53632e4008188fc31b302f88369839e
Theo Brigitte <theo.brigitte@gmail.com>
2024-11-26 23:12:32 +0100

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/TheoBrigitte/go-test/pkg/util"
+)
+
+func main() {
+	fmt.Println(util.Output)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TheoBrigitte/go-test
+
+go 1.23.1
